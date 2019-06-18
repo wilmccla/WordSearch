bc35281938e4af1533118e8519e13d3eab640260
wilmccla <46944916+wilmccla@users.noreply.github.com>
2019-06-18 14:05:51 -0700

    Add files via upload

--- WordSearch/WordSearch/WordSearch.cpp
+++ WordSearch/WordSearch/WordSearch.cpp
@@ -2,45 +2,123 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include "WordSearch.h"
 using namespace  std;
 
 int main()
 {
-	//Declaring a file that has both input and output
-	fstream KeyWords;
+	cout << "Welcome. This is my word search program that counts the number of certain keywords that are contained within my chosen paragraphs.\n";
+	cout << "Let us begin by creating the file that will contain the Keywords.\n";
+
+	//Call CreateKeyWords
+	CreateKeyWords();
+
+	cout << "Next, we shall create the file that will contain the paragraphs.\n";
+
+	//Call CreateParagraphs
+	CreateParagraphs();
+
+	cout << "Now, we will compare the Keywords to the words in the paragraphs.\n";
+
+	//Call Comparewords
+	CompareWords();
+}
+
+void CreateKeyWords()
+{
 	cout << "Creating KeyWords File...\n";
 	//Create a new text file called KeyWords
-	KeyWords.open("KeyWords.txt");
+	KeyWords.open("KeyWords.txt", fstream::out);
 	system("pause");
 
-	cout << "Adding KeyWords to file...";
-	KeyWords << "Never\n";
-	KeyWords << "You\n";
-	KeyWords << "It\n";
+	cout << "Adding KeyWords to file...\n";
+	KeyWords << "Never" << endl;
+	KeyWords << "You" << endl;
+	KeyWords << "It" << endl;
+	KeyWords << "Foes" << endl;
+	KeyWords << "Armor" << endl;
+	KeyWords << "Weakness" << endl;
+	KeyWords << "People" << endl;
+	KeyWords << "Your" << endl;
+	KeyWords << "Peace" << endl;
+	KeyWords << "Confused" << endl;
+	system("pause");
 
-	//Declaring a file that has both input and output
-	fstream Paragraphs;
+	cout << "Closing KeyWords File...\n";
+	//Close file
+	KeyWords.close();
+	system("pause");
+	return;
+}
+
+void CreateParagraphs()
+{
 	cout << "Creating Paragraphs File...\n";
 	//Creating a new text file called Paragraphs
-	Paragraphs.open("Paragraphs.txt");
+	Paragraphs.open("Paragraphs.txt", fstream::out);
 	system("pause");
 
-	cout << "\n\nAdding Paragraphs To File...\n";
+	cout << "Adding Paragraphs To File...\n";
 	//First Paragraph
-	Paragraphs << "Never forget what you are, for surely the world will not.\n";
-	Paragraphs << "Make it your strength. Then it can never be your weakness.\n";
-	Paragraphs << "Armor yourself in it, and it will never be used to hurt you.\n\n";
+	Paragraphs << "Never forget what you are, for surely the world will not." << endl;
+	Paragraphs << "Make it your strength. Then it can never be your weakness." << endl;
+	Paragraphs << "Armor yourself in it, and it will never be used to hurt you." << endl << endl;
 	//Second Paragraph
-	Paragraphs << "The common people pray for rain, healthy children, and a summer that never ends.\n";
-	Paragraphs << "It is no matter to them if the high lords play their game of thrones.\n";
-	Paragraphs << "So long as they are left in peace. They never are.\n\n";
+	Paragraphs << "The common people pray for rain, healthy children, and a summer that never ends." << endl;
+	Paragraphs << "It is no matter to them if the high lords play their game of thrones." << endl;
+	Paragraphs << "So long as they are left in peace. They never are." << endl << endl;
 	//Third Paragraph
-	Paragraphs << "Always keep your foes confused.\n";
-	Paragraphs << "If they are never certain who you are or what you want,\n";
-	Paragraphs << "they cannot know what you are like to do next.\n";
-	Paragraphs << "Sometimes the best way to baffle them is to make moves that have no purpose,\n";
-	Paragraphs << "or even seem to work against you\n\n";
+	Paragraphs << "Always keep your foes confused." << endl;
+	Paragraphs << "If they are never certain who you are or what you want," << endl;
+	Paragraphs << "they cannot know what you are like to do next." << endl;
+	Paragraphs << "Sometimes the best way to baffle them is to make moves that have no purpose," << endl;
+	Paragraphs << "or even seem to work against you." << endl << endl;
 	system("pause");
+
 	cout << "Closing Paragraphs File...";
+	//Close File
+	Paragraphs.close();
+	system("pause");
+	return;
+}
+
+void CompareWords()
+{
+	cout << "Opening KeyWords...\n";
+	KeyWords.open("KeyWords.txt");
+
+	cout << "Getting KeyWords...\n";
+	KeyWords >> word;
+	//Loop while there's still words to read in KeyWords
+	while (KeyWords.good())
+	{
+		//Add word to the vector
+		VKeyWords.push_back(word);
+		//Move on to the next word
+		KeyWords >> word;
+	}
+
+	cout << "All Keywords accounted, Closing File...\n";
+	KeyWords.close();
+
+	cout << "Opening Paragraphs...\n";
+	Paragraphs.open("Paragraphs.txt");
+
+	cout << "Reading Paragraphs...\n";
+	Paragraphs >> word;
+	while (Paragraphs.good())
+	{
+		//Add word to the vector
+		VParagraphs.push_back(word);
+		//Move on to the next word
+		Paragraphs >> word;
+	}
+
+	cout << "All Paragraphs accounted, Closing File...\n";
+	Paragraphs.close();
+
+	cout << "Comparing Files...";
 
 }
+
+
--- WordSearch/WordSearch/WordSearch.h
+++ WordSearch/WordSearch/WordSearch.h
@@ -1,3 +1,32 @@
 #pragma once
 #include <string>
+#include <vector>
 using namespace std;
+
+//Declaring files that has both input and output
+fstream KeyWords;
+fstream Paragraphs;
+
+//Declare Vectors for word comparison
+vector<string> VKeyWords;
+vector<string> VParagraphs;
+
+//Declare variable to hold the incoming word from KeyWords
+char word[];
+
+//KeyWords Counters
+int KeyWord0 = 0;
+int KeyWord1 = 0;
+int KeyWord2 = 0;
+int KeyWord3 = 0;
+int KeyWord4 = 0;
+int KeyWord5 = 0;
+int KeyWord6 = 0;
+int KeyWord7 = 0;
+int KeyWord8 = 0;
+int KeyWord9 = 0;
+
+//Declaring Methods
+void CreateKeyWords();
+void CreateParagraphs();
+void CompareWords();
